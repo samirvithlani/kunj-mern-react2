ce847bf07b68b7f5230c381f91bf2edef5f3accd
samirvithlani <samir.vithlani83955@gmail.com>
2026-01-17 12:34:27 +0530

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,21 @@ import AppRoutes from "./components/router/AppRoutes";
 function App() {
   return (
     <>
-      <div className="bg-blue-200 p-6 text-red-500">
+      <div>
         <button className="bg-[#FE7F96] rounded mt-4 p-6 text-2xl font-bold">ADD</button>
+        <div className="min-h-screen bg-gray-100 p6">
+          <div className="flex gap-4 justify-center items-center p-50">
+            <div className="bg-yellow-400 p-6 rouned shadow h-100 w-100">
+              BOX 1
+            </div>
+            <div className="bg-red-700 p-6 rouned shadow h-100 w-100">
+              BOX 2
+            </div>
+            <div className="bg-pink-900 p-6 rouned shadow h-100 w-100">
+              BOX 3
+            </div>
+          </div>
+        </div>
         <AppRoutes></AppRoutes>
       </div>
     </>
