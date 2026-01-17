4666db330d5bba5765eb406d3543fa93065d79d8
samirvithlani <samir.vithlani83955@gmail.com>
2026-01-17 12:19:03 +0530

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import AppRoutes from './components/router/AppRoutes'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import AppRoutes from "./components/router/AppRoutes";
 //import './App.css'
 
 function App() {
-  
-
   return (
     <>
-      <AppRoutes></AppRoutes>
+      <div className="bg-blue-200 p-6 text-red-500">
+        <button className="bg-[#FE7F96] rounded mt-4 p-6 text-2xl font-bold">ADD</button>
+        <AppRoutes></AppRoutes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
